4843161b01d744b2b6c8ee25d2504fb07b53986b
MelvynnHadia <69381490+MelvynnHadia@users.noreply.github.com>
2021-04-24 12:48:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+
+var firebaseConfig = {
+    
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default  firebase.firestore()                                   
